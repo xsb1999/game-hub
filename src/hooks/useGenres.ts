df7583ce6f08e864429bf9e531190b33a1f482a6
xsb1999 <18810235625@163.com>
2024-08-03 00:55:03 -0400

    Fetch the genres

---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Genre {
+  id: number;
+  name: string;
+  slug: string;
+  games_count: number;
+  image_background: string;
+}
+
+interface FetchGenreResponse {
+  count: number;
+  results: Genre[];
+}
+
+const useGenres = () => {
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+    apiClient
+      .get<FetchGenreResponse>("/genres", { signal: controller.signal })
+      .then((res) => {
+        setLoading(false);
+        setGenres(res.data.results);
+      })
+      .catch((err) => {
+        setLoading(false);
+        if (err instanceof CanceledError) {
+          return;
+        }
+        setError(err.message);
+      });
+    return () => controller.abort();
+  }, []);
+
+  return { genres, error, isLoading };
+};
+
+export default useGenres;
