d741336111f128ea33af26bcebc4563c3ce8164f
xsb1999 <18810235625@163.com>
2024-08-03 12:09:31 -0400

    Add skeleton and spinner when loading genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,26 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import {
+  Box,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
+import GenreListSkeleton from "./GenreListSkeleton";
 const GenreList = () => {
   const { data, error, isLoading } = useGenres();
+  if (error) return null;
   return (
     <>
-      {isLoading && <Spinner />}
+      {isLoading && (
+        <Box>
+          <Spinner />
+          <GenreListSkeleton />
+        </Box>
+      )}
       <List>
         {data.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
---
+++ src/components/GenreListSkeleton.tsx
@@ -0,0 +1,29 @@
+import {
+  HStack,
+  List,
+  ListItem,
+  Skeleton,
+  SkeletonCircle,
+  Text,
+  SkeletonText,
+  Box,
+} from "@chakra-ui/react";
+
+const GenreListSkeleton = () => {
+  return (
+    <List>
+      {Array.from({ length: 20 }).map((_, index) => (
+        <Box paddingY="5px">
+          <ListItem key={index}>
+            <HStack>
+              <Skeleton boxSize="32px" borderRadius={8} />
+              <Skeleton height="20px" width="100px" />
+            </HStack>
+          </ListItem>
+        </Box>
+      ))}
+    </List>
+  );
+};
+
+export default GenreListSkeleton;
