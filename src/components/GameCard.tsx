95687bace7c5ecdddd937990c2113da2e7163dc5
xsb1999 <18810235625@163.com>
2024-08-05 00:49:36 -0400

    Clean up GameCard

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -14,8 +14,7 @@ const GameCard = ({ game }: Props) => {
       <Card>
         <Image src={getCroppedImageUrl(game.background_image)} />
         <CardBody>
-          <Heading fontSize={"2xl"}>{game.name}</Heading>
-          <HStack justifyContent={"space-between"}>
+          <HStack justifyContent={"space-between"} marginBottom={3}>
             <PlatformIconsList
               platforms={
                 game.parent_platforms
@@ -25,6 +24,7 @@ const GameCard = ({ game }: Props) => {
             />
             <CriticScore score={game.metacritic} />
           </HStack>
+          <Heading fontSize={"2xl"}>{game.name}</Heading>
         </CardBody>
       </Card>
     </>
